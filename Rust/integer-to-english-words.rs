bbd72bcf5126a3240b2332dfa1d0e9da1dc572e8
Francesco Vatteroni <39797822+The90Frank@users.noreply.github.com>
2024-08-07 08:50:44 +0200

    Create integer-to-english-words.rs

---
+++ Rust/integer-to-english-words.rs
@@ -0,0 +1,79 @@
+impl Solution {
+    pub fn number_to_words(num: i32) -> String {
+        let mut ret_string : String =  "".to_owned();
+        
+        let map_unit_0_19 = vec![
+            "",
+            "One ",	        
+            "Two ",	    
+            "Three ",	
+            "Four ",
+            "Five ",
+            "Six ",    
+            "Seven ",	
+            "Eight ",
+            "Nine ",
+            "Ten ",    
+            "Eleven ",	
+            "Twelve ",
+            "Thirteen ",	
+            "Fourteen ",
+            "Fifteen ",	    
+            "Sixteen ",	    
+            "Seventeen ",	
+            "Eighteen ",	
+            "Nineteen "
+        ];
+
+        let map_tenth_0_90 = vec![
+            "",
+            "Ten ",
+            "Twenty ",
+            "Thirty ",
+            "Forty ",	
+            "Fifty ",	
+            "Sixty ",
+            "Seventy ",
+            "Eighty ",
+            "Ninety "
+        ];
+
+        let chunk_num = vec![
+            (num / 1000000000) % 1000,
+            (num / 1000000) % 1000,
+            (num / 1000) % 1000,
+            (num / 1) % 1000,
+        ];
+
+        let string_1000 = vec![
+            "Billion ",
+            "Million ", 
+            "Thousand ",
+            ""
+        ];
+
+        for i in 0..chunk_num.len() {
+            let cur_num = chunk_num[i];
+            if cur_num != 0 {
+                if cur_num >= 100 {
+                   ret_string.push_str(map_unit_0_19[ (cur_num/100) as usize ]);
+                   ret_string.push_str("Hundred ");
+                }
+
+                if cur_num % 100 < 20 {
+                    ret_string.push_str(map_unit_0_19[ (cur_num%100) as usize ]);
+                } else {
+                    ret_string.push_str(map_tenth_0_90[ ((cur_num%100) / 10) as usize ]);
+                    ret_string.push_str(map_unit_0_19[ (cur_num%10) as usize ]);
+                }
+
+                ret_string.push_str(string_1000[i]);
+            }
+        }
+
+        if ret_string == "" {
+            return "Zero".to_string();
+        }
+        return ret_string.trim().to_string();
+    }
+}
